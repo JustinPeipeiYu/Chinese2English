a984705f9cd040277fc6d5d89271c025ca3a9189
Justin Yu <justinyu6866@gmail.com>
2024-05-07 10:56:19 -0400

    Day 6 Add consonants to search word + display in label

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,6 +25,7 @@ namespace Idiom_Translator
     /// </summary>
     public partial class MainWindow : Window
     {
+        string searchWord="";
         public MainWindow()
         {
             InitializeComponent();
@@ -49,7 +50,7 @@ namespace Idiom_Translator
                         e.Add(c);
                     }
                 }
-                lblPinyin.Content = e.Count();
+                //lblPinyin.Content = e.Count();
             }
         }
 
@@ -81,12 +82,14 @@ namespace Idiom_Translator
 
         private void btnQ_Click(object sender, RoutedEventArgs e)
         {
-
+            searchWord += "q";
+            lblPinyin.Content=searchWord;
         }
 
         private void btnW_Click(object sender, RoutedEventArgs e)
         {
-
+            searchWord += "w";
+            lblPinyin.Content = searchWord;
         }
 
         private void btnE_Click(object sender, RoutedEventArgs e)
@@ -96,17 +99,20 @@ namespace Idiom_Translator
 
         private void btnR_Click(object sender, RoutedEventArgs e)
         {
-
+            searchWord += "r";
+            lblPinyin.Content = searchWord;
         }
 
         private void btnT_Click(object sender, RoutedEventArgs e)
         {
-
+            searchWord += "t";
+            lblPinyin.Content = searchWord;
         }
 
         private void btnY_Click(object sender, RoutedEventArgs e)
         {
-
+            searchWord += "y";
+            lblPinyin.Content = searchWord;
         }
 
         private void btnU_Click(object sender, RoutedEventArgs e)
@@ -126,7 +132,8 @@ namespace Idiom_Translator
 
         private void btnP_Click(object sender, RoutedEventArgs e)
         {
-
+            searchWord += "p";
+            lblPinyin.Content = searchWord;
         }
 
         private void btnA_Click(object sender, RoutedEventArgs e)
@@ -136,82 +143,98 @@ namespace Idiom_Translator
 
         private void btnS_Click(object sender, RoutedEventArgs e)
         {
-
+            searchWord += "s";
+            lblPinyin.Content = searchWord;
         }
 
         private void btnD_Click(object sender, RoutedEventArgs e)
         {
-
+            searchWord += "d";
+            lblPinyin.Content = searchWord;
         }
 
         private void btnF_Click(object sender, RoutedEventArgs e)
         {
-
+            searchWord += "f";
+            lblPinyin.Content = searchWord;
         }
 
         private void btnG_Click(object sender, RoutedEventArgs e)
         {
-
+            searchWord += "g";
+            lblPinyin.Content = searchWord;
         }
 
         private void btnH_Click(object sender, RoutedEventArgs e)
         {
-
+            searchWord += "h";
+            lblPinyin.Content = searchWord;
         }
 
         private void btnJ_Click(object sender, RoutedEventArgs e)
         {
-
+            searchWord += "j";
+            lblPinyin.Content = searchWord;
         }
 
         private void btnK_Click(object sender, RoutedEventArgs e)
         {
-
+            searchWord += "k";
+            lblPinyin.Content = searchWord;
         }
 
         private void btnL_Click(object sender, RoutedEventArgs e)
         {
-
+            searchWord += "l";
+            lblPinyin.Content = searchWord;
         }
 
         private void btnZ_Click(object sender, RoutedEventArgs e)
         {
-
+            searchWord += "z";
+            lblPinyin.Content = searchWord;
         }
 
         private void btnX_Click(object sender, RoutedEventArgs e)
         {
-
+            searchWord += "x";
+            lblPinyin.Content = searchWord;
         }
 
         private void btnC_Click(object sender, RoutedEventArgs e)
         {
-
+            searchWord += "c";
+            lblPinyin.Content = searchWord;
         }
 
         private void btnV_Click(object sender, RoutedEventArgs e)
         {
-
+            searchWord += "v";
+            lblPinyin.Content = searchWord;
         }
 
         private void btnB_Click(object sender, RoutedEventArgs e)
         {
-
+            searchWord += "b";
+            lblPinyin.Content = searchWord;
         }
 
         private void btnN_Click(object sender, RoutedEventArgs e)
         {
-
+            searchWord += "n";
+            lblPinyin.Content = searchWord;
         }
 
         private void btnM_Click(object sender, RoutedEventArgs e)
         {
-
+            searchWord += "m";
+            lblPinyin.Content = searchWord;
         }
 
         private void btnUdot_Click(object sender, RoutedEventArgs e)
         {
-
+            searchWord += "ü";
+            lblPinyin.Content = searchWord; 
         }
 
         private void btnExclamation_Click(object sender, RoutedEventArgs e)
